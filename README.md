f048f2b3b9c52ba1021c6b4c570cea7802fd48cc
Trevor Stevens <trevor@sociallytied.com>
2015-06-11 12:30:00 -0400

    Updated code structure and added command line args

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-## Tanks In Real Life
+## Tanks In Real Life - Device Component
+
+#### Installation
+* Install [Python 2.7](https://www.python.org/)
+* Navigate to project directory
+* Run `pip install -r requirements.txt`
+* Run `python -r <roombaPort>` to run locally or `python -r <roombaPort> -p` to run against production
--- client.py
+++ client.py
@@ -1,30 +1,52 @@
 #!/usr/bin/python
+
 from socketIO_client import SocketIO, BaseNamespace
+import sys
+import getopt
 import serial
 import time
 
-serialPort = '/dev/ttyUSB0'
-# serialPort = 'COM5'
+# const variables
+localServerUrl = 'http://localhost'
+hostedServerUrl = 'https://tanks-irl.azurewebsites.net'
 playerNumber = 1
-
-serialPort = serialPort
-ser = serial.Serial(port=serialPort, baudrate=115200) # open serial connection
-
-ser.write([128]) # start
-time.sleep(.1) # per create 2 spec, wait until roomba can switch mode
-ser.write([132]) # enter safe mode
-time.sleep(.1) # per create 2 spec, wait until roomba can switch mode
-
-ser.write('\xA4' + ' P%s ' % playerNumber) # write the player string to the LCD
-ser.write('\x8B\x04\x00\xFF') # change the home LED color to green
-time.sleep(1)
+playerDisplayName = '\xA4' + ' P%s ' % playerNumber
+mainLEDGreen = '\x8B\x04\x00\xFF'
 
 # move commands
 UP = '\x89\x00\xC8\x80\x00'
 DOWN = '\x89\xFF\x38\x80\x00'
 LEFT = '\x89\x00\xC8\x00\x01'
 RIGHT = '\x89\x00\xC8\xFF\xFF'
+STOP = '\x89\x00\x00\x80\x00'
 
+# parses the command line arguements into variables. example of valid roombaPorts: '/dev/ttyUSB0' and 'COM5'
+def parseArgs(argv):
+    roombaPort = None
+    production = False
+    clientRunSyntax = 'client.py -r <roombaPort> [-p]'
+
+    try:
+        opts, args = getopt.getopt(argv,"hr:s",["roombaPort=", "production="])
+    except getopt.GetoptError:
+        print(clientRunSyntax)
+        sys.exit(-1)
+    if len(opts) <= 0:
+        print(clientRunSyntax)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(clientRunSyntax)
+            sys.exit(2)
+        elif opt in ("-r", "--roombaPort"):
+            roombaPort = arg
+        elif opt in ("-p", "--production"):
+            production = True
+        else:
+            print('opt =', opt)
+            print(clientRunSyntax)
+            sys.exit(2)
+    return (roombaPort, production)
 
 # Standard events
 class Namespace(BaseNamespace):
@@ -32,7 +54,7 @@ class Namespace(BaseNamespace):
         print('[Connection Established]')
 
 # Custom events
-def on_move(self, *args):
+def onMove(self, *args):
     print(self)
     if self == 'UP':
         ser.write(UP)
@@ -43,17 +65,33 @@ def on_move(self, *args):
     elif self == 'RIGHT':
         ser.write(RIGHT)
     else:
-        print 'wtf'
         return
     time.sleep(.2)
-    ser.write('\x89\x00\x00\x80\x00')
+    ser.write(STOP)
 
-def on_exit(self, *args):
+def onExit(self, *args):
     ser.write([173])
     ser.close() # close the serial connection
+    sys.exit(1)
 
-# socketIO = SocketIO('http://localhost', 1337, Namespace)
-socketIO = SocketIO('https://tanks-irl.azurewebsites.net', verify=False)
-socketIO.on('MOVE', on_move)
-socketIO.on('EXIT', on_exit)
-socketIO.wait()
+def main(argv):
+    (roombaPort, production) = parseArgs(argv)
+    ser = serial.Serial(port=roombaPort, baudrate=115200) # open serial connection
+
+    ser.write([128]) # start
+    time.sleep(.1) # per create 2 spec, wait until roomba can switch mode
+    ser.write([132]) # enter safe mode
+    time.sleep(.1) # per create 2 spec, wait until roomba can switch mode
+
+    ser.write(playerDisplayName) # write the player string to the LCD
+    ser.write(mainLEDGreen) # change the home LED color to green
+    time.sleep(1)
+
+    socketIO = SocketIO(hostedServerUrl, verify=False) if production else ocketIO(localServerUrl, 1337, Namespace)
+    
+    socketIO.on('MOVE', onMove)
+    socketIO.on('EXIT', onExit)
+    socketIO.wait()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
