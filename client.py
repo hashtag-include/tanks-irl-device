63b69b86da06d624661377d971d626bee3353976
Trevor Stevens <trevor@sociallytied.com>
2015-06-11 14:40:52 -0400

    Abstracted out constants, restructured code, added exception handling

--- client.py
+++ client.py
@@ -1,96 +1,87 @@
 #!/usr/bin/python
 
 from socketIO_client import SocketIO, BaseNamespace
+from serial import Serial, SerialException
+import const
 import sys
 import getopt
-import serial
 import time
 
-# const variables
-localServerUrl = 'http://localhost'
-hostedServerUrl = 'https://tanks-irl.azurewebsites.net'
-playerNumber = 1
-playerDisplayName = '\xA4' + ' P%s ' % playerNumber
-mainLEDGreen = '\x8B\x04\x00\xFF'
-
-# move commands
-UP = '\x89\x00\xC8\x80\x00'
-DOWN = '\x89\xFF\x38\x80\x00'
-LEFT = '\x89\x00\xC8\x00\x01'
-RIGHT = '\x89\x00\xC8\xFF\xFF'
-STOP = '\x89\x00\x00\x80\x00'
-
 # parses the command line arguements into variables. example of valid roombaPorts: '/dev/ttyUSB0' and 'COM5'
-def parseArgs(argv):
+def parse_args(argv):
     roombaPort = None
     production = False
-    clientRunSyntax = 'client.py -r <roombaPort> [-p]'
+    syntax = 'client.py -r <roombaPort> [-p]'
 
     try:
-        opts, args = getopt.getopt(argv,"hr:s",["roombaPort=", "production="])
+        opts, args = getopt.getopt(argv,"h:r:p",["roombaPort=", "production="])
     except getopt.GetoptError:
-        print(clientRunSyntax)
+        print(syntax)
         sys.exit(-1)
     if len(opts) <= 0:
-        print(clientRunSyntax)
-        sys.exit(2)
+        print(syntax)
+        sys.exit(-1)
     for opt, arg in opts:
-        if opt == '-h':
-            print(clientRunSyntax)
-            sys.exit(2)
-        elif opt in ("-r", "--roombaPort"):
+        if opt in ('-h', '--help'):
+            print(syntax)
+            sys.exit(-1)
+        elif opt in ('-r', '--roombaPort'):
             roombaPort = arg
-        elif opt in ("-p", "--production"):
+        elif opt in ('-p', '--production'):
             production = True
-        else:
-            print('opt =', opt)
-            print(clientRunSyntax)
-            sys.exit(2)
     return (roombaPort, production)
 
-# Standard events
-class Namespace(BaseNamespace):
+# events in the CommandControl namespace
+class CommandControl(BaseNamespace):
     def on_connect(self):
         print('[Connection Established]')
 
-# Custom events
-def onMove(self, *args):
+# event when move command is issued
+def on_move(self, *args):
     print(self)
     if self == 'UP':
-        ser.write(UP)
+        ser.write(const.SEQ_MOVE_UP)
     elif self == 'DOWN':
-        ser.write(DOWN)
+        ser.write(const.SEQ_MOVE_DOWN)
     elif self == 'LEFT':
-        ser.write(LEFT)
+        ser.write(const.SEQ_MOVE_LEFT)
     elif self == 'RIGHT':
-        ser.write(RIGHT)
+        ser.write(const.SEQ_MOVE_RIGHT)
     else:
         return
     time.sleep(.2)
-    ser.write(STOP)
+    ser.write(const.SEQ_MOVE_STOP)
 
-def onExit(self, *args):
-    ser.write([173])
+# event when exit command is issued
+def on_exit(self, *args):
+    ser.write(const.SEQ_STOP)
+    time.sleep(1)
     ser.close() # close the serial connection
     sys.exit(1)
 
 def main(argv):
-    (roombaPort, production) = parseArgs(argv)
-    ser = serial.Serial(port=roombaPort, baudrate=115200) # open serial connection
+    (roombaPort, production) = parse_args(argv)
 
-    ser.write([128]) # start
-    time.sleep(.1) # per create 2 spec, wait until roomba can switch mode
-    ser.write([132]) # enter safe mode
-    time.sleep(.1) # per create 2 spec, wait until roomba can switch mode
+    global ser # define ser is unscoped global variable
+    try:
+        ser = Serial(port = roombaPort, baudrate = const.ROOMBA_BAUD_RATE) # open serial connection
+    except SerialException:
+        print('Serial connection to Roomba over %s could not be established' % roombaPort)
+        sys.exit(-1)
 
-    ser.write(playerDisplayName) # write the player string to the LCD
-    ser.write(mainLEDGreen) # change the home LED color to green
+    ser.write(const.SEQ_START) # start
     time.sleep(1)
+    ser.write(const.SEQ_MODE_SAFE) # enter safe mode
+    time.sleep(0.2)
 
-    socketIO = SocketIO(hostedServerUrl, verify=False) if production else ocketIO(localServerUrl, 1337, Namespace)
+    ser.write(const.SEQ_PLAYER_NUMBER) # write the player string to the LCD
+    ser.write(const.SEQ_MAIN_LED_GREEN) # change the home LED color to green
+    time.sleep(0.2)
+
+    socketIO = SocketIO(const.PROD_SERVER_HOST, const.PROD_SERVER_PORT, CommandControl, verify = False) if production else SocketIO(const.DEV_SERVER_HOST, const.DEV_SERVER_PORT, CommandControl)
     
-    socketIO.on('MOVE', onMove)
-    socketIO.on('EXIT', onExit)
+    socketIO.on('MOVE', on_move)
+    socketIO.on('EXIT', on_exit)
     socketIO.wait()
 
 if __name__ == "__main__":
---
+++ const.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+### Constants
+
+# socket server
+DEV_SERVER_HOST = 'http://localhost'
+DEV_SERVER_PORT = 1337
+PROD_SERVER_HOST = 'https://tanks-irl.azurewebsites.net'
+PROD_SERVER_PORT = 443
+
+# roomba constants
+ROOMBA_BAUD_RATE = 115200
+
+# main roomba sequences
+SEQ_START = [128]
+SEQ_STOP = [173]
+SEQ_MODE_SAFE = [132]
+
+# misc roomba sequences
+SEQ_PLAYER_NUMBER = '\xA4' + ' P%s ' % 1
+SEQ_MAIN_LED_GREEN = '\x8B\x04\x00\xFF'
+
+# roomba move sequences
+SEQ_MOVE_UP = '\x89\x00\xC8\x80\x00' # move straight @ 200 mm/s
+SEQ_MOVE_DOWN = '\x89\xFF\x38\x80\x00' # move straight @ -200 mm/s
+SEQ_MOVE_LEFT = '\x89\x00\xC8\x00\x01' # move left @ 200 mm/s
+SEQ_MOVE_RIGHT = '\x89\x00\xC8\xFF\xFF' # move right @ 200 mm/s
+SEQ_MOVE_STOP = '\x89\x00\x00\x80\x00' # move straight @ 0 mm/s
